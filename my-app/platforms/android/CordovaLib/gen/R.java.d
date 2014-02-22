b1f0fbe81ec8520b0f73a6ec68a158ab90332934
Allen Turner <tedturner29@ufl.edu>
2014-02-22 17:09:39 -0500

    my-app added, adb deleted

---
+++ my-app/platforms/android/CordovaLib/gen/R.java.d
@@ -0,0 +1,1 @@
+ : C:\my-app\platforms\android\CordovaLib\bin\AndroidManifest.xml \
---
+++ my-app/platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\my-app\platforms\android\gen\com\phonegap\helloworld\R.java \
+ : C:\my-app\platforms\android\res\drawable\icon.png \
+C:\my-app\platforms\android\res\drawable-hdpi\icon.png \
+C:\my-app\platforms\android\res\drawable-ldpi\icon.png \
+C:\my-app\platforms\android\res\drawable-mdpi\icon.png \
+C:\my-app\platforms\android\res\drawable-xhdpi\icon.png \
+C:\my-app\platforms\android\res\values\strings.xml \
+C:\my-app\platforms\android\res\xml\config.xml \
+C:\my-app\platforms\android\bin\AndroidManifest.xml \
